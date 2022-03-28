ef8843da1414a23e9d1c743ff4db8589230c1886
BladeofJesus <maparole0212@gmail.com>
2022-03-28 00:14:03 -0400

    change

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,7 +42,7 @@ public class TestGreeter {
   }
   
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingSpeaker() {
     String someone = "World, Thanks you making me Happy willy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
